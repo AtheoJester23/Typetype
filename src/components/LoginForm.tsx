77f2518f27577885cc93e7e3214a725fc1df82e1
AtheoJester23 <atheojester@gmail.com>
2025-11-17 18:21:26 +0800

    style: add show password button

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,6 +1,21 @@
-import { Link } from "react-router-dom";
+import { Link, type HTMLFormMethod } from "react-router-dom";
+import { Eye, EyeClosed } from "lucide-react";
+import { useState } from "react";
 
 const LoginForm = () => {
+    const [show, setShow] = useState(false)
+
+    const handleLogin = async(e: React.FormEvent<HTMLFormElement>) =>{
+        e.preventDefault();
+
+        const formData = new FormData(e.currentTarget);
+        const email = formData.get("email") as string
+        const password = formData.get("password") as string;
+
+        console.log("Email: ", email);
+        console.log("Password: ", password);
+    }
+
     return (  
         <form className="formLogin">
             <h1 className="text-center font-bold text-3xl text-[rgb(23,23,23)]">Login</h1>
@@ -12,7 +27,10 @@ const LoginForm = () => {
                 </div>
                 <div className="flex flex-col">
                     <label htmlFor="password" className="text-gray-500">Password: </label>
-                    <input type="text" name="password" id="password" className="border border-gray-500 rounded p-[10px]" placeholder="Password"/>
+                    <div className="relative">
+                        <input type={show ? "text" : "password"} name="password" id="password" className="border border-gray-500 rounded p-[10px] w-full" placeholder="Password" autoComplete="off"/>
+                        <button type="button" className="absolute right-3 top-3" onClick={() => setShow(prev => !prev)}>{show ? <Eye className="text-gray-500"/> : <EyeClosed className="text-gray-500"/>}</button>
+                    </div>
                 </div>
 
             </div>
