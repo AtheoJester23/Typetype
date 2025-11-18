b2c08ea887e557cfbacde2a6c561ede2cbf1bc25
AtheoJester23 <atheojester@gmail.com>
2025-11-19 01:53:48 +0800

    style: restyle form inputs

--- src/components/SignupForm.tsx
+++ src/components/SignupForm.tsx
@@ -1,26 +1,46 @@
-import { AtSign, Lock, LockKeyhole, User } from "lucide-react";
+import { AtSign, Eye, EyeClosed, Lock, LockKeyhole, User } from "lucide-react";
+import { useState } from "react";
 
 const SignupForm = () => {
+    const [show, setShow] = useState(false)
+
+    const handleSignIn = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        const formData = new FormData(e.currentTarget);
+        const username = formData.get("username") as String;
+        const email = formData.get("email") as String;
+        const password = formData.get("createPassword") as String;
+        const confirmPassword = formData.get("confirmPassword") as String;
+
+        console.log("Data", JSON.stringify({username, email, password, confirmPassword}))
+    }
+
     return (  
-        <form className="formSignup">
+        <form className="formSignup" onSubmit={(e) => handleSignIn(e)}>
             <h1 className="text-center font-bold text-4xl text-[rgb(23,23,23)]">Sign Up</h1>
 
             <div className="flex flex-col gap-2">
                 <div className="input-box">
+                    <User className="text-[rgb(23,23,23)]"/>
                     <input type="text" name="username" id="username" className="border border-gray-500 px-3 py-2 rounded" placeholder="Username" autoComplete="off"/>
-                    <User className="absolute right-2 top-2 text-gray-500"/>
                 </div>
                 <div className="input-box">
+                    <AtSign className="text-[rgb(23,23,23)]"/>
                     <input type="text" name="email" id="email" className="border border-gray-500 px-3 py-2 rounded" placeholder="Email" autoComplete="off"/>
-                    <AtSign className="absolute right-2 top-2 text-gray-500"/>
                 </div>
                 <div className="input-box">
-                    <input type="password" name="createPassword" id="createPassword" className="border border-gray-500 px-3 py-2 rounded" placeholder="Create Password" autoComplete="off"/>
-                    <Lock className="absolute right-2 top-2 text-gray-500"/>
+                    <Lock className="text-[rgb(23,23,23)]"/>
+                    <input type={show ? "text" : "password"} name="createPassword" id="createPassword" className="border border-gray-500 px-3 py-2 rounded" placeholder="Create Password" autoComplete="off"/>
                 </div>
                 <div className="input-box">
-                    <input type="password" name="confirmPassword" id="confirmPassword" className="border border-gray-500 px-3 py-2 rounded" placeholder="Confirm Password" autoComplete="off"/>
-                    <LockKeyhole className="absolute right-2 top-2 text-gray-500"/>
+                    <Lock className="text-[rgb(23,23,23)]"/>
+                    <div className="passwordBox">
+                        <input type={show ? "text" : "password"} name="confirmPassword" id="confirmPassword" className="border border-gray-500 px-3 py-2 rounded" placeholder="Confirm Password" autoComplete="off"/>
+                        <button type="button" onClick={() => setShow(prev => !prev)}>
+                            {show ? <Eye className="absolute right-2 top-2.5 text-gray-500"/> : <EyeClosed className="absolute right-2 top-2.5 text-gray-500"/>}
+                        </button>
+                    </div>
                 </div>
             </div>
 
